33879e1ab80d5c137a3fda18bbae43ba4494e16a
AnaisBonin <anaisboninfr@gmail.com>
2021-09-29 19:17:29 +0200

    Added first files, with chark and js content : cow

---
+++ index.js
@@ -0,0 +1,14 @@
+const importInfo = require('./information');
+const chalk = require('chalk');
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I am sexy and I know it",
+    e : "- O",
+    T : "U "
+}));
+
+for (let i = 0; i < 3; i++){
+    console.log(`${chalk.greenBright("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\")}`);
+}
---
+++ information.js
@@ -0,0 +1,6 @@
+const student = {
+    name: "Anaïs",
+    campus: "Remote",
+}
+
+module.exports = student ;
